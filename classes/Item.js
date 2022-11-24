2138ec1ef40f749631dc879fcbea595e05d378b8
emily <nakanodepaula@gmail.com>
2022-11-24 15:33:28 -0300

    use of abstract class to taxItem and Item

--- classes/Item.js
+++ classes/Item.js
@@ -3,5 +3,9 @@ export default class Item {
         this.category = category
         this.mark = mark
         this.price = price
+
+        if (this.constructor == Item) {
+            throw new Error("Abstract classes can't be instantiated.");
+        }
     }
 }
--- classes/TaxItem.js
+++ classes/TaxItem.js
@@ -3,7 +3,11 @@ import Item from "./Item.js";
 export default class TaxItem extends Item {
     constructor(category, mark, price) {
         super(category, mark, price)
-    }
+
+        if (this.constructor == TaxItem) {
+            throw new Error("Abstract classes can't be instantiated.");
+        }
+    } 
 
     calculateTax(tax) {
         const charge = this.price * tax;
